2e137864418bd2753b1ea827c7f38ec09f7ecc6e
Jazel Canseco <jazel.canseco@gmail.com>
2021-07-25 21:59:43 -0700

    Revert "Avoid loading autoloaded files"
    This reverts commit d942f39ed4dcaf26185163d5f80a5c5a5b4eec2b.
    
    This commit was cherry-picked from github.com/tarjoilija/zgen/pull/132
    as a way to fix github.com/zsh-users/zsh-completions/issues/809.
    
    However, this commit seems to have problems [1], and a more correct
    solution exists [2].
    
    [1] https://github.com/tarjoilija/zgen/pull/132#issuecomment-886182213
    [2] https://github.com/jandamm/zgenom/pull/61

--- zgen.zsh
+++ zgen.zsh
@@ -148,9 +148,8 @@ zgen-clone() {
 
 -zgen-source() {
     local file="${1}"
-    local base=$(basename "${file}")
 
-    if [[ ! "${base}" =~ "^_" && ! "${ZGEN_LOADED[@]}" =~ "${file}" ]]; then
+    if [[ ! "${ZGEN_LOADED[@]}" =~ "${file}" ]]; then
         ZGEN_LOADED+=("${file}")
         source "${file}"
 
